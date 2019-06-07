1f9ba4c4716e256680a8099b3c362c212d1ebae6
“Patrick <pctennislessons@gmail.com>
2019-06-07 11:17:03 -0600

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,12 @@
+require "pry"
+
 def count_elements(array)
+  # binding.pry
+  count_hash = Hash.new(0)
+  array.each do |creatures|
+    count_hash[creatures] += 1
+  end
+  count_hash
   # code goes here
 end
  
